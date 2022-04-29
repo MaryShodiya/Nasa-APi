be7a81f08c3f1844b35d27d72afcb30c1994e832
MaryShodiya <marysho57@gmail.com>
2022-04-29 16:29:51 +0100

    modified the html file

--- nasa.js
+++ nasa.js
@@ -6,16 +6,12 @@ document.querySelector('button').addEventListener('click', getDate);
       .then(res => res.json()) // parse response as JSON
       .then(data => {
     console.log(data)
-if(data.media_type === 'image') {
     document.querySelector('img').src = data.hdurl;
-    
-}else if(data.media_type === 'video'){
   document.querySelector('iframe').src = data.url;
-}
 document.querySelector('h3').innerText = data.explanation;   
       })
       .catch(err => {
           console.log(`error ${err}`)
       }); 
        
-  }
+    }
