2b57fd61a49db514dd66c67e0486b4f07cd0189e
MaryShodiya <marysho57@gmail.com>
2022-04-29 15:58:27 +0100

    added a Nasa APi

---
+++ nasa.js
@@ -0,0 +1,21 @@
+document.querySelector('button').addEventListener('click', getDate);
+  function getDate() {
+    let pastData = document.querySelector('input').value ;
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=ncDIWQXNBO5y9mKuwu9cVcBf1Dm19esPMjow9qy3&date=${pastData}`)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+    console.log(data)
+if(data.media_type === 'image') {
+    document.querySelector('img').src = data.hdurl;
+    
+}else if(data.media_type === 'video'){
+  document.querySelector('iframe').src = data.url;
+}
+document.querySelector('h3').innerText = data.explanation;   
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      }); 
+       
+  }
